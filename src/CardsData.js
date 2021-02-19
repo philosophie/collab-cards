4fbd4fb6fedefd18c0802e10f82a2a453cc5d05a
Jamie Caloras <jamison.caloras@gmail.com>
2021-02-19 18:40:29 -0500

    Add diverge copy

--- src/CardsData.js
+++ src/CardsData.js
@@ -1,76 +1,61 @@
 const cards=[
   {
-  "prompt": "What has kept you from it in the past?",
-  "details": "What stumbling blocks, if any, have slowed you in the past? Are they resolved? If not, how might they be?"
+  "prompt": "Make it more accessible.",
+  "details": ""
   }, {
-  "prompt": "Why now?",
-  "details": "You chose today to work on it. What is special about this moment in time?"
+  "prompt": "Make it more exclusive.",
+  "details": ""
   }, {
-  "prompt": "How would you describe it to a 5-year-old?",
-  "details": "Use this as an opportunity to think about what you're doing the simplest terms possible. Is everyone on the same page?"
+  "prompt": "Steal from a competitor.",
+  "details": ""
   }, {
-  "prompt": "Create a mission statement.",
-  "details": "Spend time silently drafting a mission statement for it. Then, read your statement to the group. Do you share a vision?"
+  "prompt": "Try something no one has.",
+  "details": ""
   }, {
-  "prompt": "Identify one improvement that could encourage joy.",
-  "details": "If you can make it more fun, why not? Help someone else enjoy the journey."
+  "prompt": "Put people before pixels.",
+  "details": ""
   }, {
-  "prompt": "What is an accepted limitation that you could break?",
-  "details": "What conventions in your field could you challenge with this work?"
+  "prompt": "Think globally.",
+  "details": ""
   }, {
-  "prompt": "What is your hypothesis?",
-  "details": "What are you proposing and how will you know if it’s working?"
+  "prompt": "Keep it local.",
+  "details": ""
   }, {
-  "prompt": "Consider how a Teacher-Author might feel about it.",
-  "details": "Take a moment and challenge each other to think about the user's experience."
+  "prompt": "Merge two very different ideas.",
+  "details": ""
   }, {
-  "prompt": "Engage with empathy.",
-  "details": "Spend some time stepping into someone else’s shoes."
+  "prompt": "What is the worst idea you can think of?",
+  "details": ""
   }, {
-  "prompt": "Whose voice is this meeting missing?",
-  "details": "By taking time to think about who is not in the room, you can build for diversity and inclusion and improve your outcomes. Could you benefit from the expertise of a member of another team? Would hearing from a Teacher-Author be helpful? What about someone from an unrepresented demographic?"
+  "prompt": "Do less, but better.",
+  "details": ""
   }, {
-  "prompt": "What if it leaves behind leftovers or a mess that needs to be cleaned?",
-  "details": "Think about the potential loose ends of your proposal. What are the trade-offs? What structures need to be in place to ensure that your work is maintained?"
+  "prompt": "How will they do it in the future?",
+  "details": ""
   }, {
-  "prompt": "What if it was fair to any person?",
-  "details": "Is it possible to make it more equitable? Could it ever be entirely equitable?"
+  "prompt": "How did they do it in the past?",
+  "details": ""
   }, {
-  "prompt": "What if it built trust?",
-  "details": "How could this tool strengthen trust between all parties?"
+  "prompt": "Make it more outrageous.",
+  "details": ""
   }, {
-  "prompt": "What if not everyone had to like it?",
-  "details": "Are any unnecessary constraints getting in your way?"
+  "prompt": "Make it more subtle.",
+  "details": ""
   }, {
-  "prompt": "Let the newest member of the team answer the next question first.",
-  "details": "Pick a new card and allow the newest member of the team to take the lead."
+  "prompt": "Believe in magic.",
+  "details": ""
   }, {
-  "prompt": "Choose someone to answer the next question.",
-  "details": "Select another member of your team to take the lead on the next question."
+  "prompt": "Emphasize the worst part.",
+  "details": ""
   }, {
-  "prompt": "Identify something that you might need help with.",
-  "details": "Take a moment to consider the limitations of your group. What help might you need and who might be able to offer it?"
+  "prompt": "Emphasize the best part.",
+  "details": ""
   }, {
-  "prompt": "Draw it in 10 seconds.",
-  "details": "Grab a piece of paper and time yourselves. Share your drawings. What can you learn from each other’s work?"
+  "prompt": "Make it more emotional.",
+  "details": ""
   }, {
-  "prompt": "What if you are a new user?",
-  "details": "Consider the perspective of a new user. What might the user enjoy? What might the user find challenging?"
-  }, {
-  "prompt": "What would Simon Cowell have to say about it?",
-  "details": "“In every single way that was just everything I hated.” – Simon Cowell"
-  }, {
-  "prompt": "Spend 30 seconds in silence.",
-  "details": "Use this time to close your eyes, meditate, stretch, or rest. "
-  }, {
-  "prompt": "What if you had no control over how it was used?",
-  "details": "Imagine that you gave up control of it. What benefits could come from that experience for you? For the User?"
-  }, {
-  "prompt": "What if you valued quality over quantity?",
-  "details": "Does it need to be big? Does it need to be frequent? Does it need to be loud?"
-  }, {
-  "prompt": "Is it accessible?",
-  "details": "Think about who might not be able to use it. Could you make some changes so that it works for more people?"
+  "prompt": "Make it more sterile.",
+  "details": ""
   }
 ]
 export default cards
